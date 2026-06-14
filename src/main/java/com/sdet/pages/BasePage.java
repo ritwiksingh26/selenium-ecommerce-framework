4d585c1dee4dab66f39876e8f3064a5e54cc1d29
ritwiksingh26 <83443734+ritwiksingh26@users.noreply.github.com>
2026-06-14 20:13:40 +0530

    Implement basePage (#1)

---
+++ src/main/java/com/sdet/pages/BasePage.java
@@ -0,0 +1,51 @@
+package com.sdet.pages;
+
+import com.sdet.base.DriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class BasePage {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage() {
+        this.driver = DriverManager.getDriver();
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    protected void click(WebElement element){
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+    }
+
+    protected void type(WebElement element, String text){
+        wait.until(ExpectedConditions.visibilityOf(element));
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected String getText(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
+        return element.getText();
+    }
+
+    protected void waitForUrl(String urlFragment){
+        wait.until(ExpectedConditions.urlContains(urlFragment));
+    }
+
+    protected boolean isElementPresent(By locator){
+        try{
+            driver.findElement(locator);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
